b0a84edb4f934f330157f763743a0beddc3e76f4
Logan Lepage <logan.lepage@free.fr>
2016-12-08 15:07:13 +0100

    refactoring views

--- src/initiatice/AdminBundle/Controller/TeacherController.php
+++ src/initiatice/AdminBundle/Controller/TeacherController.php
@@ -9,7 +9,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Form\Extension\Core\Type\TimeType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 
 class TeacherController extends Controller
 {
